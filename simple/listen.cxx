bc95e452339fe604f7f837956669cd9dc5c0aa43
Wang <wanxwang@uiowa.edu>
2018-01-10 12:27:59 -0600

    transported variables: position,tangent,lateral, vel, acc, latAccel, angularVel

--- simple/listen.cxx
+++ simple/listen.cxx
@@ -30,28 +30,28 @@ int keybrdTick(void);
 
 void DtRtiShutdownHandler(const char * label, void* finished)
 {
-   //This callback may occur multiple times, since it gets called
-   //every time we make an RTI call with an error. So, we mark this
-   //as finished and make sure that we only signal once.
-   int* finishedInt = (int*)finished;
-   if (*finishedInt)
-   {
-      DtWarn << "Shutting down: " << label << std::endl;
-   }
-   *finishedInt = 0; //Order a shutdown;
+	//This callback may occur multiple times, since it gets called
+	//every time we make an RTI call with an error. So, we mark this
+	//as finished and make sure that we only signal once.
+	int* finishedInt = (int*)finished;
+	if (*finishedInt)
+	{
+		DtWarn << "Shutting down: " << label << std::endl;
+	}
+	*finishedInt = 0; //Order a shutdown;
 }
 
 // A callback for when a DtReflectedEnvironmentProcess object is updated.
 void printEnvProc(DtReflectedEnvironmentProcess* obj, void* userData)
 {
-   obj->epsr()->printData();
+	obj->epsr()->printData();
 }
 
 // A callback for when a new DtReflectedEnvironmentProcess object is added.
 void envProcAddedCb(DtReflectedEnvironmentProcess* obj, void* userData)
 {
-   // register a callback for when an existing object is updated.
-   obj->addPostUpdateCallback(printEnvProc, userData);
+	// register a callback for when an existing object is updated.
+	obj->addPostUpdateCallback(printEnvProc, userData);
 }
 
 
@@ -59,151 +59,155 @@ void envProcAddedCb(DtReflectedEnvironmentProcess* obj, void* userData)
 // Define a callback to process fire interactions.
 void fireCb(DtFireInteraction* fire, void* /*usr*/)
 {
-   std::cout << "Fire Interaction from "
-      <<  fire->attackerId().string() << std::endl;
+	std::cout << "Fire Interaction from "
+		<<  fire->attackerId().string() << std::endl;
 }
 
 int main(int argc, char** argv)
 {
-   DtINIT_MINIDUMPER( "Listen" );
+	DtINIT_MINIDUMPER( "Listen" );
 
-   try
-   {
+	try
+	{
 
-      // Create an exercise conn initializer. This will parse the command
-      // line, as well as parse an mtl file with the same name as this application.
-      DtVrlApplicationInitializer appInit(argc, argv, "VR-Link Listen");
+		// Create an exercise conn initializer. This will parse the command
+		// line, as well as parse an mtl file with the same name as this application.
+		DtVrlApplicationInitializer appInit(argc, argv, "VR-Link Listen");
 
 #if DtDIS
-      // Please note, this is the only protocol specific code in this example. For
-      // HLA there is no network connection, so there is no concept of AsyncIO at the
-      // VR-Link level.
-      appInit.setUseAsynchIO( true );
-      appInit.setDisVersionToSend( 7 );
+		// Please note, this is the only protocol specific code in this example. For
+		// HLA there is no network connection, so there is no concept of AsyncIO at the
+		// VR-Link level.
+		appInit.setUseAsynchIO( true );
+		appInit.setDisVersionToSend( 7 );
 #else
-      // Set the default FED File, executable, and version
-      appInit.setFedFileName( DtDefaultRpr2Fom );
-      appInit.setExecName( "MAK-RPR-2.0" );
-      appInit.setRprFomVersion( 2.0 );
-      appInit.setRprFomRevision( 1 );
+		// Set the default FED File, executable, and version
+		appInit.setFedFileName( DtDefaultRpr2Fom );
+		appInit.setExecName( "MAK-RPR-2.0" );
+		appInit.setRprFomVersion( 2.0 );
+		appInit.setRprFomRevision( 1 );
 #ifdef DtHLA_1516_EVOLVED
-      std::vector<DtString> fomModules;
-      std::vector<DtString> fomModuleFromCommandLine;
-      appInit.fomModules(fomModuleFromCommandLine);
-      fomModules.push_back("MAK-VRFExt-1_evolved.xml");
-      fomModules.push_back("MAK-DIGuy-2_evolved.xml");
-      fomModules.push_back("MAK-LgrControl-1_evolved.xml");
-      fomModules.push_back("MAK-VRFAggregate-1_evolved.xml");
-      fomModules.push_back("MAK-DynamicTerrain-1_evolved.xml");
-      fomModules.insert(fomModules.end(), fomModuleFromCommandLine.begin(), fomModuleFromCommandLine.end());
-      appInit.setFomModules(fomModules);
+		std::vector<DtString> fomModules;
+		std::vector<DtString> fomModuleFromCommandLine;
+		appInit.fomModules(fomModuleFromCommandLine);
+		fomModules.push_back("MAK-VRFExt-1_evolved.xml");
+		fomModules.push_back("MAK-DIGuy-2_evolved.xml");
+		fomModules.push_back("MAK-LgrControl-1_evolved.xml");
+		fomModules.push_back("MAK-VRFAggregate-1_evolved.xml");
+		fomModules.push_back("MAK-DynamicTerrain-1_evolved.xml");
+		fomModules.insert(fomModules.end(), fomModuleFromCommandLine.begin(), fomModuleFromCommandLine.end());
+		appInit.setFomModules(fomModules);
 #endif
 #endif
 
-      // Process the command line. This will actually set values in the
-      // DtVrlApplicationInitializer class and should be called before
-      // creating the DtExerciseConn instance.
-      appInit.parseCmdLine();
+		// Process the command line. This will actually set values in the
+		// DtVrlApplicationInitializer class and should be called before
+		// creating the DtExerciseConn instance.
+		appInit.parseCmdLine();
 
-      DtExerciseConn::InitializationStatus status = DtExerciseConn::DtINIT_SUCCESS;
+		DtExerciseConn::InitializationStatus status = DtExerciseConn::DtINIT_SUCCESS;
 
-      // The DtExerciseConn instance is perhaps the most important class
-      // in any VR-Link exercise. Think of it as the hub which holds
-      // everything else together. Technically this instance is what's
-      // responsible for connecting this federate to another federate via
-      // a network, or an RTI. After exConn is created, and if no error
-      // has occurred this simulator will be a live federate.
-      DtExerciseConn exConn(appInit, &status);
+		// The DtExerciseConn instance is perhaps the most important class
+		// in any VR-Link exercise. Think of it as the hub which holds
+		// everything else together. Technically this instance is what's
+		// responsible for connecting this federate to another federate via
+		// a network, or an RTI. After exConn is created, and if no error
+		// has occurred this simulator will be a live federate.
+		DtExerciseConn exConn(appInit, &status);
 
-      if (status != 0)
-      {
-         std::cout << "Error creating exercise connection." << std::endl;
-         return -1;
-      }
+		if (status != 0)
+		{
+			std::cout << "Error creating exercise connection." << std::endl;
+			return -1;
+		}
 
-      DtReflectedEnvironmentProcessList relEnv(&exConn);
-      relEnv.addEnvironmentProcessAdditionCallback(envProcAddedCb, NULL);
+		DtReflectedEnvironmentProcessList relEnv(&exConn);
+		relEnv.addEnvironmentProcessAdditionCallback(envProcAddedCb, NULL);
 
-      // Register a callback to handle fire interactions.
-      DtFireInteraction::addCallback(&exConn, fireCb, NULL);
+		// Register a callback to handle fire interactions.
+		DtFireInteraction::addCallback(&exConn, fireCb, NULL);
 
-      // Create an object to manage entities that we hear about
-      // on the network.
-      DtReflectedEntityList rel(&exConn);
+		// Create an object to manage entities that we hear about
+		// on the network.
+		DtReflectedEntityList rel(&exConn);
 
-      // Initialize VR-Link time.
-      DtClock* clock = exConn.clock();
+		// Initialize VR-Link time.
+		DtClock* clock = exConn.clock();
 
-      //Shutdown handler in order to smoothly end if the HLA exercise shuts down
-      int forever = 1;
+		//Shutdown handler in order to smoothly end if the HLA exercise shuts down
+		int forever = 1;
 #ifdef DtHLA
-      exConn.addRtiErrorCb(&DtRtiShutdownHandler, &forever);
+		exConn.addRtiErrorCb(&DtRtiShutdownHandler, &forever);
 #endif
-      DtTime dt = 1.0/60.0; //60 hz
-      DtTime simTime = 0; //in seconds
+		DtTime dt = 1.0/60.0; //60 hz
+		DtTime simTime = 0; //in seconds
+		DWORD time_n = GetTickCount();
 
-      while (forever && simTime <= 600.0)
-      {
-         // Check if user hit 'q' to quit.
-         if (keybrdTick() == -1)
-            break;
+		while (forever && simTime <= 600.0)
+		{
+			// Check if user hit 'q' to quit.
+			if (keybrdTick() == -1)
+				break;
 
-         //simTime = clock->elapsedRealTime();
-         // Tell VR-Link the current value of simulation time.
-         clock->setSimTime(simTime);
+			//simTime = clock->elapsedRealTime();
+			// Tell VR-Link the current value of simulation time.
+			clock->setSimTime(simTime);
 
-         // Process any incoming messages.
-         exConn.drainInput();
+			// Process any incoming messages.
+			exConn.drainInput();
 
-         // Find the first entity in the reflected entity list.
-         DtReflectedEntity *first = rel.first();
+			// Find the first entity in the reflected entity list.
+			DtReflectedEntity *first = rel.first();
 
-         if (first)
-         {
-            // Grab its state repository, where we can inspect its data.
-            DtEntityStateRepository *esr = first->entityStateRep();
+			if (first)
+			{
+				// Grab its state repository, where we can inspect its data.
+				DtEntityStateRepository *esr = first->entityStateRep();
 
-            // Create a topographic view on the state repository, so we
-            // can look at position information in topographic coordinates.
-            double refLatitude  = DtDeg2Rad(  35.699760);
-            double refLongitude = DtDeg2Rad(-121.326577);
-            DtTopoView topoView(esr, refLatitude, refLongitude);
+				// Create a topographic view on the state repository, so we
+				// can look at position information in topographic coordinates.
+				double refLatitude  = DtDeg2Rad(  35.699760);
+				double refLongitude = DtDeg2Rad(-121.326577);
+				DtTopoView topoView(esr, refLatitude, refLongitude);
 
-            ExternalDriverStateTran stateTran;
+				ExternalDriverStateTran stateTran;
 
-            stateTran.vel = topoView.velocity();
-            stateTran.acc = topoView.acceleration();
-            stateTran.ori = topoView.orientation();
-            stateTran.rot = topoView.rotationalVelocity();
-            stateTran.loc = topoView.location();
-            Logout(simTime, stateTran);
+				stateTran.vel = topoView.velocity();
+				stateTran.acc = topoView.acceleration();
+				stateTran.ori = topoView.orientation();
+				stateTran.rot = topoView.rotationalVelocity();
+				stateTran.loc = topoView.location();
+				DWORD time_np = GetTickCount();
+				Logout(simTime, time_np - time_n, stateTran);
 
-            ExternalDriverState stateSim;
-            Transform(stateTran, stateSim);
-            Logout(simTime, stateSim);
-            // Print the position.
-            // Since it returns a DtString, we need to force it to const char*
-            // with a cast.
-            //std::cout << "Position of first entity: "
-            //   << topoView.location().string() << " time: " << simTime << std::endl;
+				ExternalDriverState stateSim;
+				Transform(stateTran, stateSim);
+				Logout(simTime, time_np - time_n, stateSim);
 
-         }
-         simTime += dt;
-         // Sleep till next iteration.
-         DtSleep(simTime - clock->elapsedRealTime());
-      }
-   }
-   DtCATCH_AND_WARN(std::cout);
-   return 0;
+				time_n = time_np;
+				// Print the position.
+				// Since it returns a DtString, we need to force it to const char*
+				// with a cast.
+				//std::cout << "Position of first entity: "
+				//   << topoView.location().string() << " time: " << simTime << std::endl;
+
+			}
+			simTime += dt;
+			// Sleep till next iteration.
+			DtSleep(simTime - clock->elapsedRealTime());
+		}
+	}
+	DtCATCH_AND_WARN(std::cout);
+	return 0;
 }
 
 int keybrdTick()
 {
-   char *keyPtr = DtPollBlockingInputLine();
-   if (keyPtr && (*keyPtr == 'q' || *keyPtr == 'Q'))
-      return -1;
-   else
-      return 0;
+	char *keyPtr = DtPollBlockingInputLine();
+	if (keyPtr && (*keyPtr == 'q' || *keyPtr == 'Q'))
+		return -1;
+	else
+		return 0;
 }
 
 
--- simple/talk.cxx
+++ simple/talk.cxx
@@ -28,157 +28,160 @@
 
 void DtRtiShutdownHandler(const char * label, void* finished)
 {
-   //This callback may occur multiple times, since it gets called
-   //every time we make an RTI call with an error. So, we mark this
-   //as finished and make sure that we only signal once.
-   int* finishedInt = (int*)finished;
-   if (*finishedInt)
-   {
-      DtWarn << "Shutting down: " << label << std::endl;
-   }
-   *finishedInt = 0; //Order a shutdown;
+	//This callback may occur multiple times, since it gets called
+	//every time we make an RTI call with an error. So, we mark this
+	//as finished and make sure that we only signal once.
+	int* finishedInt = (int*)finished;
+	if (*finishedInt)
+	{
+		DtWarn << "Shutting down: " << label << std::endl;
+	}
+	*finishedInt = 0; //Order a shutdown;
 }
 
 int main( int argc, char* argv[] )
 {
-   // Used for error handling
-   DtINIT_MINIDUMPER( "Talk" );
+	// Used for error handling
+	DtINIT_MINIDUMPER( "Talk" );
 
-   try
-   {
-      // Create a connection to the exercise or federation execution.
-      DtVrlApplicationInitializer appInit( argc, argv, "VR-Link Talk" );
+	try
+	{
+		// Create a connection to the exercise or federation execution.
+		DtVrlApplicationInitializer appInit( argc, argv, "VR-Link Talk" );
 
 #if DtDIS
-      // Change some defaults. Please note that this is DIS specific code.
-      // Notice how this is the only line of DIS specific code in this example,
-      // everything else is using the protocol independent API.
-      appInit.setUseAsynchIO( true );
-      appInit.setDisVersionToSend( 7 );
+		// Change some defaults. Please note that this is DIS specific code.
+		// Notice how this is the only line of DIS specific code in this example,
+		// everything else is using the protocol independent API.
+		appInit.setUseAsynchIO( true );
+		appInit.setDisVersionToSend( 7 );
 #else
-      appInit.setFedFileName( DtDefaultRpr2Fom );
-      appInit.setExecName( "MAK-RPR-2.0" );
-      appInit.setRprFomVersion( 2.0 );
-      appInit.setRprFomRevision( 1 );
+		appInit.setFedFileName( DtDefaultRpr2Fom );
+		appInit.setExecName( "MAK-RPR-2.0" );
+		appInit.setRprFomVersion( 2.0 );
+		appInit.setRprFomRevision( 1 );
 
 #ifdef DtHLA_1516_EVOLVED
-      std::vector<DtString> fomModules;
-      std::vector<DtString> fomModuleFromCommandLine;
-      appInit.fomModules(fomModuleFromCommandLine);
-      fomModules.push_back("MAK-VRFExt-1_evolved.xml");
-      fomModules.push_back("MAK-DIGuy-2_evolved.xml");
-      fomModules.push_back("MAK-LgrControl-1_evolved.xml");
-      fomModules.push_back("MAK-VRFAggregate-1_evolved.xml");
-      fomModules.push_back("MAK-DynamicTerrain-1_evolved.xml");
-      fomModules.insert(fomModules.end(), fomModuleFromCommandLine.begin(), fomModuleFromCommandLine.end());
-      appInit.setFomModules(fomModules);
+		std::vector<DtString> fomModules;
+		std::vector<DtString> fomModuleFromCommandLine;
+		appInit.fomModules(fomModuleFromCommandLine);
+		fomModules.push_back("MAK-VRFExt-1_evolved.xml");
+		fomModules.push_back("MAK-DIGuy-2_evolved.xml");
+		fomModules.push_back("MAK-LgrControl-1_evolved.xml");
+		fomModules.push_back("MAK-VRFAggregate-1_evolved.xml");
+		fomModules.push_back("MAK-DynamicTerrain-1_evolved.xml");
+		fomModules.insert(fomModules.end(), fomModuleFromCommandLine.begin(), fomModuleFromCommandLine.end());
+		appInit.setFomModules(fomModules);
 #endif
 #endif
 
-      // Process the command line. This will actually set values in the
-      // DtVrlApplicationInitializer class and should be called before
-      // creating the DtExerciseConn instance.
-      appInit.parseCmdLine();
+		// Process the command line. This will actually set values in the
+		// DtVrlApplicationInitializer class and should be called before
+		// creating the DtExerciseConn instance.
+		appInit.parseCmdLine();
 
-      DtExerciseConn::InitializationStatus status = DtExerciseConn::DtINIT_SUCCESS;
+		DtExerciseConn::InitializationStatus status = DtExerciseConn::DtINIT_SUCCESS;
 
-      // The DtExerciseConn instance is perhaps the most important class
-      // in any VR-Link exercise. Think of it as the hub which holds
-      // everything else together. Technically this instance is what's
-      // responsible for connecting this federate to another federate via
-      // a network, or an RTI. After exConn is created, and if no error
-      // has occurred this simulator will be a live federate.
-      DtExerciseConn exConn(appInit, &status);
+		// The DtExerciseConn instance is perhaps the most important class
+		// in any VR-Link exercise. Think of it as the hub which holds
+		// everything else together. Technically this instance is what's
+		// responsible for connecting this federate to another federate via
+		// a network, or an RTI. After exConn is created, and if no error
+		// has occurred this simulator will be a live federate.
+		DtExerciseConn exConn(appInit, &status);
 
-      if (status != 0)
-      {
-         std::cout << "Error creating exercise connection." << std::endl;
-         return -1;
-      }
+		if (status != 0)
+		{
+			std::cout << "Error creating exercise connection." << std::endl;
+			return -1;
+		}
 
-      DtEntityType f18Type(DtPlatform, DtPlatformDomainAir,
-                           DtUnitedStates, DtFighter, DtF18, 0, 0);
+		DtEntityType f18Type(DtPlatform, DtPlatformDomainAir,
+									DtUnitedStates, DtFighter, DtF18, 0, 0);
 
-      // Create an entity publisher for the entity we are simulating.
-      DtEntityPublisher entityPub(f18Type, &exConn,
-                                  DtDrDrmRvw, DtForceFriendly,
-                                  DtEntityPublisher::guiseSameAsType());
+		// Create an entity publisher for the entity we are simulating.
+		DtEntityPublisher entityPub(f18Type, &exConn,
+											 DtDrDrmRvw, DtForceFriendly,
+											 DtEntityPublisher::guiseSameAsType());
 
-      // Hold on to the entity's state repository, where we can set data.
-      DtEntityStateRepository* esr = entityPub.entityStateRep();
+		// Hold on to the entity's state repository, where we can set data.
+		DtEntityStateRepository* esr = entityPub.entityStateRep();
 
-      // Create a topographic view on the state repository, so we
-      // can set position information in topographic coordinates.
-      double refLatitude  = DtDeg2Rad(   35.699760 );
-      double refLongitude = DtDeg2Rad( -121.326577 );
-      DtTopoView topoView(esr, refLatitude, refLongitude);
+		// Create a topographic view on the state repository, so we
+		// can set position information in topographic coordinates.
+		double refLatitude  = DtDeg2Rad(   35.699760 );
+		double refLongitude = DtDeg2Rad( -121.326577 );
+		DtTopoView topoView(esr, refLatitude, refLongitude);
 
-      // We can use the ESR to set state.
-      esr->setMarkingText("VR-Link");
-      topoView.setOrientation(DtTaitBryan(0.0, 0.0, 0.0));
+		// We can use the ESR to set state.
+		esr->setMarkingText("VR-Link");
+		topoView.setOrientation(DtTaitBryan(0.0, 0.0, 0.0));
 
-      // Initialize VR-Link time.
-      DtClock* clock = exConn.clock();
+		// Initialize VR-Link time.
+		DtClock* clock = exConn.clock();
 
-      DtVector position(0, 0, -100);
-      DtVector32 velocity(20, 0, 0);
+		DtVector position(0, 0, -100);
+		DtVector32 velocity(20, 0, 0);
 
-      //Shutdown handler in order to smoothly end if the HLA exercise shuts down
-      int forever = 1;
+		//Shutdown handler in order to smoothly end if the HLA exercise shuts down
+		int forever = 1;
 #ifdef DtHLA
-      exConn. addRtiErrorCb(&DtRtiShutdownHandler, &forever);
+		exConn. addRtiErrorCb(&DtRtiShutdownHandler, &forever);
 #endif
 
-      // Send a Fire Interaction.
-      DtFireInteraction fire;
-      fire.setAttackerId(entityPub.globalId());
-      exConn.sendStamped(fire);
+		// Send a Fire Interaction.
+		DtFireInteraction fire;
+		fire.setAttackerId(entityPub.globalId());
+		exConn.sendStamped(fire);
 
-      // Main loop
-      DtTime dt = 1.0/60.0; //60 hz
-      DtTime simTime = 0; //in seconds
-      clock->init();
+		// Main loop
+		DtTime dt = 1.0/60.0; //60 hz
+		DtTime simTime = 0; //in seconds
+		DWORD time_n = GetTickCount();
+		clock->init();
 
-      ExternalDriverState stateSim;
-      memset(&stateSim, 0, sizeof(ExternalDriverState));
-      ExternalDriverStateTran stateTran;
-      memset(&stateTran, 0, sizeof(ExternalDriverStateTran));
-
-
-      while (forever && simTime <= 600.0)
-      {
-         // Tell VR-Link the current value of simulation time.
-         clock->setSimTime(simTime);
-         // Process any incoming messages.
-         exConn.drainInput();
-
-         mockDyna(simTime, stateSim);
-         Logout(simTime, stateSim);
-         Transform(stateSim, stateTran);
-         Logout(simTime, stateTran);
-
-         // Set the current position information.
-         topoView.setVelocity(stateTran.vel);
-         topoView.setAcceleration(stateTran.acc);
-         topoView.setOrientation(stateTran.ori);
-         topoView.setRotationalVelocity(stateTran.rot);
-         topoView.setLocation(stateTran.loc);
+		ExternalDriverState stateSim;
+		memset(&stateSim, 0, sizeof(ExternalDriverState));
+		ExternalDriverStateTran stateTran;
+		memset(&stateTran, 0, sizeof(ExternalDriverStateTran));
 
 
+		while (forever && simTime <= 600.0)
+		{
+			// Tell VR-Link the current value of simulation time.
+			clock->setSimTime(simTime);
+			// Process any incoming messages.
+			exConn.drainInput();
 
-         // Call tick, which insures that any data that needs to be
-         // updated is sent.
-         entityPub.tick();
+			mockDyna(simTime, stateSim);
+			DWORD time_np = GetTickCount();
+			Logout(simTime, time_np - time_n, stateSim);
+			Transform(stateSim, stateTran);
+			Logout(simTime, time_np - time_n, stateTran);
+			time_n = time_np;
 
-         // Set up for next iteration.
-         position[0] += velocity[0] * dt;
+			// Set the current position information.
+			topoView.setVelocity(stateTran.vel);
+			topoView.setAcceleration(stateTran.acc);
+			topoView.setOrientation(stateTran.ori);
+			topoView.setRotationalVelocity(stateTran.rot);
+			topoView.setLocation(stateTran.loc);
 
-         simTime     += dt;
 
-         // Wait till real time equals simulation time of next step.
-         DtSleep(simTime - clock->elapsedRealTime());
-      }
-   }
-   DtCATCH_AND_WARN(std::cout);
-   return 0;
+
+			// Call tick, which insures that any data that needs to be
+			// updated is sent.
+			entityPub.tick();
+
+			// Set up for next iteration.
+			position[0] += velocity[0] * dt;
+
+			simTime     += dt;
+
+			// Wait till real time equals simulation time of next step.
+			DtSleep(simTime - clock->elapsedRealTime());
+		}
+	}
+	DtCATCH_AND_WARN(std::cout);
+	return 0;
 }
